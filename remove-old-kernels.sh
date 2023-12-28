b3ce32e36d2b02e3f2826711ba8d2f850c7839eb
Dailey, Alaric <alaricd@pengdows.com>
2023-12-27 18:32:38 -0600

    preventing linux-image-amd64 from being removed

--- remove-old-kernels.sh
+++ remove-old-kernels.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 PATH=/bin:/usr/sbin:/sbin:/usr/local/sbin
-dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | grep -v `uname -r` | grep -v libc-dev | xargs sudo apt-get -y purge
-
+BASE=`apt-mark showinstall | grep ^linux-image | grep -v ^linux-image-[0-9]`
+dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | grep -v `uname -r` | grep -v libc-dev | grep -v $BASE | xargs sudo apt-get -y purge
