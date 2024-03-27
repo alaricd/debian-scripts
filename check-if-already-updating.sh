9c145336307ccbf2a1fc35f3268d21d0f64e66ab
Dailey, Alaric <alaricd@pengdows.com>
2024-03-27 12:50:24 -0500

    updating shell

--- check-if-already-updating.sh
+++ check-if-already-updating.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 # Check if apt-get or dpkg is running
 if pidof apt-get > /dev/null || pidof dpkg > /dev/null; then
     echo "An update process is already running. Exiting..."
