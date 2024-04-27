f83a9fab57b6758694bcf20a5b2bdebd4b80cb4d
Dailey, Alaric <alaricd@pengdows.com>
2024-04-27 09:08:25 -0500

    retain manually installed packages

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 PATH=/bin:/usr/sbin:/sbin:/usr/local/sbin
+SAVED_PACKAGES=$(apt-mark showmanual)
 dpkg --configure -a && \
 apt-get update && \
 apt-get dist-upgrade -y && \
-apt-get autoremove -y --purge && \
-apt-get purge $(deborphan --guess-all)  -y
+sudo apt-get purge $(deborphan --guess-all | grep -v "$(apt-mark showmanual)" | tr '\n' ' ') -y
+
