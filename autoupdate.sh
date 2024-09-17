07f74a78efbad5eaaef3dd9a370638b95235cbf6
Dailey, Alaric <alaricd@pengdows.com>
2024-09-17 11:06:59 -0500

    better detectino of dpkg needing to be run

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 PATH=/bin:/usr/sbin:/sbin:/usr/local/sbin
-dpkg --configure -a && \
+export DEBIAN_FRONTEND=noninteractive
+
+dpkg --configure -a --force-confdef --force-confold && \
 apt-get update && \
 apt-get dist-upgrade -y && \
-sudo apt-get purge $(deborphan --guess-all | grep -v "$(apt-mark showmanual)" | tr '\n' ' ') -y && \
+apt-get purge $(deborphan --guess-all | grep -v "$(apt-mark showmanual)" | tr '\n' ' ') -y && \
 /bin/check-requirements.sh
--- check-requirements.sh
+++ check-requirements.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
-
+export DEBIAN_FRONTEND=noninteractive
 # List of packages to check and install if necessary
-packages=("deborphan" "needrestart")
+packages=("nc", "sed", "deborphan" "needrestart")
 
 # Loop through the list of packages and install them if they are not already installed
 for pkg in "${packages[@]}"; do
     if ! dpkg -s "$pkg" >/dev/null 2>&1; then
         echo "$pkg is not installed. Installing..."
         sudo apt-get install -y "$pkg"
+        if [ $? -ne 0 ]; then
+            echo "Error installing $pkg. Exiting."
+            exit 1
+        fi
+        if [ "$pkg" == "needrestart" ]; then
+            echo "configuring needrestart..."
+            sed -i 's/#\$nrconf{restart} = .*/\$nrconf{restart} = "a";/' /etc/needrestart/needrestart.conf
+
+        fi
     else
         echo "$pkg is already installed."
     fi
