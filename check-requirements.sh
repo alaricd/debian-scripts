f9b517233db85f78260c158cb118aac4d47c2105
Alaric Dailey <alaricd@pengdows.com>
2024-12-05 08:51:03 -0600

    updating check-requirements.sh for other debian variants

--- check-requirements.sh
+++ check-requirements.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 export DEBIAN_FRONTEND=noninteractive
 # List of packages to check and install if necessary
-packages=("nc", "sed", "deborphan" "needrestart")
+
+# Detect the distribution
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+fi
+
+# Define package lists specific to each distribution
+if [ "$ID" == "kali" ]; then
+    # Kali-specific package list (netcat is already included by default)
+    packages=("nc" "sed" "deborphan" "needrestart")
+elif [ "$ID" == "ubuntu" ]; then
+    # Ubuntu-specific package list
+    packages=("netcat-openbsd" "sed" "deborphan" "needrestart")
+elif [ "$ID" == "debian" ]; then
+    # Debian-specific package list
+    packages=("netcat" "sed" "deborphan" "needrestart")
+else
+    echo "Unsupported distribution: $ID"
+    exit 1
+fi
 
 # Loop through the list of packages and install them if they are not already installed
 for pkg in "${packages[@]}"; do
     if ! dpkg -s "$pkg" >/dev/null 2>&1; then
         echo "$pkg is not installed. Installing..."
-        sudo apt-get install -y "$pkg"
+        apt-get install -y "$pkg"
         if [ $? -ne 0 ]; then
             echo "Error installing $pkg. Exiting."
             exit 1
